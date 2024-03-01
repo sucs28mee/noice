b7b05c7999d75dea0c6f6d65b789e5f42826972b
sucss28mee <adam.dudkiewicz@outlook.com>
2024-03-01 16:27:22 +0100

    Removed random stuff.

--- examples/image.rs
+++ examples/image.rs
@@ -3,9 +3,6 @@ use std::ops::Add;
 use noice::{Interpolation, Noisifier, Perlin};
 
 fn main() {
-    let count = 10000;
-    let average = (0..count).map(|x| noice::prng(x)).fold(0.0, Add::add) / count as f64;
-    println!("{average}");
     Perlin::new(Interpolation::Cubic)
         .gen_image(1000, 1000, 10)
         .save("examples/noise.png")
